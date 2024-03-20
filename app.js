f3511da59d005cc6f8d2d57cae106344f5745118
Danial <danbaluanov@bk.ru>
2024-03-20 18:29:05 +0500

    new

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } 
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.cont');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
